c5d99ba967509b0d6d5bc40dbf5a942a50858fd8
Momo L'As <momolas@users.noreply.github.com>
2024-11-19 10:31:12 +0100

    Initial Commit

---
+++ NOISYSTRESS/NOISYSTRESSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOISYSTRESSApp.swift
+//  NOISYSTRESS
+//
+//  Created by Mo on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NOISYSTRESSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
